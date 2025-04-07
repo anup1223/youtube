85e87b79ae5826b4ecfd598501149e9bfe4fb19b
Anup Kumar Tiwari <anupkumartiwari020@gmail.com>
2025-04-07 21:58:35 +0530

    refactor user routes: reorganize imports and enhance route definitions for user functionalities

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -112,7 +112,7 @@ const loginUser = asyncHandler(async (req, res) => {
   // 5. access and refresh token
   // 6. send cookie
 
-  const { email, username, password } = req.body;
+  const { username, email ,password } = req.body;
 
   if (!username && !email) {
     // both email and username is required
@@ -168,8 +168,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1, // this removes the field from the document
       },
     },
     {
